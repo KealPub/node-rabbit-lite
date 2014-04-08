46e005337e966a5af36f7b89bdfc2efbd6179abe
Evgeniy <e.ivanov@fbs.com>
2014-04-08 19:27:00 +0400

    create method getConnect AND add tests for After connected

--- test/Rabbit.js
+++ test/Rabbit.js
@@ -2,52 +2,97 @@
  * Created by Evgeniy on 20.03.14.
  */
 
-var RabbitMQ = require("../lib/rabbit").createConnect("amqp://"+process.env.HOST || "localhost").RabbitMQ
-    , should = require("should");
+var should = require("should")
+    , rabbit = require("../lib/rabbit")
+    , _ = require("underscore");
 
-describe("RabbitMQ", function(){
+var testCase = {
+
+    publish: function(Queue){
+       return function(done){
+
+           Queue.subscribe(function(messasge){
+               messasge.message.should.be.eql({test: "test"});
+               Queue.unsubscribe();
+               done();
+           }, function(){
+               Queue.publish({test: "test"});
+           });
+
+       }
+    },
+
+    publishAndReply: function(Queue){
+        return function(done){
+
+            Queue.subscribe(function(message){
+                if(message.message.test == "reply"){
+                    message.reply({test: "test"});
+                }else if(message.message.test == "test"){
+                    Queue.unsubscribe();
+                    done();
+                }
+            }, function(){
+                Queue.publish({test: "reply"}, {replyTo: "test.queue"});
+            });
+
+        }
+    },
+
+    publishAndAck: function(Queue){
+
+        return function(done){
+
+            Queue.subscribe(function(message){
+                message.acked();
+                Queue.unsubscribe();
+                done();
+            }, function(){
+                Queue.publish({test: "ack"});
+            }, true);
+
+        }
+
+    }
+
+}
 
 
-    var Queue = new RabbitMQ("test.queue");
+describe("Before connection", function(){
+    var rb = rabbit.createConnect("amqp://"+process.env.HOST || "localhost");
+    var Queue = new rb.RabbitMQ("test.queue");
 
-    it("#publish", function(done){
+    it("#publish", testCase["publish"](Queue));
 
-        Queue.subscribe(function(messasge){
-            messasge.message.should.be.eql({test: "test"});
-            Queue.unsubscribe();
-            done();
-        }, function(){
-            Queue.publish({test: "test"});
-        });
+    it("#publish and Reply", testCase["publishAndReply"](Queue));
 
-    });
+    it("#publish and ack", testCase["publishAndAck"](Queue));
 
-    it("#publish and Reply", function(done){
-
-          Queue.subscribe(function(message){
-              if(message.message.test == "reply"){
-                  message.reply({test: "test"});
-              }else if(message.message.test == "test"){
-                  Queue.unsubscribe();
-                  done();
-              }
-          }, function(){
-              Queue.publish({test: "reply"}, {replyTo: "test.queue"});
+    after(function(done){
+          rabbit.closeConnect(function(){
+              done();
           });
-
     });
 
-    it("#publish and ack", function(done){
 
-        Queue.subscribe(function(message){
-           message.acked();
-           Queue.unsubscribe();
-           done();
-        }, function(){
-            Queue.publish({test: "ack"});
-        }, true);
+});
 
+describe("After connection", function(){
+    var Queue = {};
+
+    before(function(done){
+         rabbit.createConnect("amqp://"+process.env.HOST || "localhost", function(err){
+             if(err) return done(err);
+
+             _.extend(Queue, new rabbit.RabbitMQ("test.queue"));
+             done();
+         });
     });
 
+    it("#publish", testCase["publish"](Queue));
+
+    it("#publish and Reply", testCase["publishAndReply"](Queue));
+
+    it("#publish and ack", testCase["publishAndAck"](Queue));
 
 });
